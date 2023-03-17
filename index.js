ebdce5b926d181fc634d787cd64947940eb90a3c
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-03-17 14:56:25 +0300

    Done with Lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph){
+    cats.push('Ralph')
+    return cats
+}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift('Bob')
+    return cats
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+
+function appendCat(Broom){
+    let moreCats = [...cats, 'Broom']
+    return moreCats
+}
+
+function prependCat(Arnold){
+    let moreCats = ['Arnold',...cats]
+    return moreCats
+}
+
+function removeLastCat(){
+    let differentCats = cats.slice(0,cats.length-1)
+    return differentCats
+}
+
+function removeFirstCat(){
+    let differentCats = cats.slice(1)
+    return differentCats
+}
